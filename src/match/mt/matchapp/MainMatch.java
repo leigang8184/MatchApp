aa9b3b5c84ffd451d7b82a72bcf8a6d1b2eb08ff
abidepan <bidepan1991@gmail.com>
2013-09-02 22:32:37 +0800

    001 main
    001

---
+++ src/match/mt/matchapp/MainMatch.java
@@ -0,0 +1,226 @@
+package match.mt.matchapp;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.app.Activity;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+public class MainMatch extends Activity implements OnGestureListener, OnTouchListener {
+	
+	private ViewFlipper viewFlipper;
+	private boolean showNext = true;
+	private boolean isRun = true;
+	private int currentPage = 0;
+	private final int SHOW_NEXT = 0011;
+	private static final int FLING_MIN_DISTANCE = 50;
+	private static final int FLING_MIN_VELOCITY = 0;
+	private GestureDetector mGestureDetector;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_match_layout);
+		
+        viewFlipper = (ViewFlipper) findViewById(R.id.main_viewflipper);
+        mGestureDetector = new GestureDetector(this);
+        viewFlipper.setOnTouchListener(this);
+        viewFlipper.setLongClickable(true);
+        viewFlipper.setOnClickListener(clickListener);
+        displayRatio_selelct(currentPage);
+        
+        Main_ScrollView myScrollView = (Main_ScrollView) findViewById(R.id.viewflipper_scrollview);
+        myScrollView.setOnTouchListener(onTouchListener);
+        myScrollView.setGestureDetector(mGestureDetector);
+        
+        thread.start();
+	}
+	
+
+
+	private OnClickListener clickListener = new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			toastInfo("µã»÷ÊÂ¼þ");
+		}
+	};
+	
+    private OnTouchListener onTouchListener = new OnTouchListener() {
+		
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			// TODO Auto-generated method stub
+			return mGestureDetector.onTouchEvent(event);
+		}
+	};
+	
+	Handler mHandler = new Handler(){
+
+		@Override
+		public void handleMessage(Message msg) {
+			// TODO Auto-generated method stub
+			switch (msg.what) {
+			case SHOW_NEXT:
+				if (showNext) {
+					showNextView();
+				} else {
+					showPreviousView();
+				}
+				break;
+
+			default:
+				break;
+			}
+		}
+    };
+
+    Thread thread = new Thread(){
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			while(isRun){
+				try {
+					Thread.sleep(1000 * 5);
+					Message msg = new Message();
+					msg.what = SHOW_NEXT;
+					mHandler.sendMessage(msg);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	};
+
+	@Override
+	public boolean onTouch(View arg0, MotionEvent event) {
+		// TODO Auto-generated method stub
+		return mGestureDetector.onTouchEvent(event);
+	}
+
+	@Override
+	public boolean onDown(MotionEvent arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+			float velocityY) {
+		// TODO Auto-generated method stub
+		Log.e("view", "onFling");
+		if (e1.getX() - e2.getX()> FLING_MIN_DISTANCE  
+                && Math.abs(velocityX) > FLING_MIN_VELOCITY ) {
+			Log.e("fling", "left");
+			showNextView();
+			showNext = true;
+//			return true;
+		} else if (e2.getX() - e1.getX() > FLING_MIN_DISTANCE  
+                && Math.abs(velocityX) > FLING_MIN_VELOCITY){
+			Log.e("fling", "right");
+			showPreviousView();
+			showNext = false;
+//			return true;
+		}
+		return false;
+		
+	}
+
+	@Override
+	public void onLongPress(MotionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean onScroll(MotionEvent arg0, MotionEvent arg1, float arg2,
+			float arg3) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void onShowPress(MotionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean onSingleTapUp(MotionEvent arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	private void showNextView(){
+
+		viewFlipper.setInAnimation(AnimationUtils.loadAnimation(this, R.anim.push_left_in));
+		viewFlipper.setOutAnimation(AnimationUtils.loadAnimation(this, R.anim.push_left_out));		
+		viewFlipper.showNext();
+		currentPage ++;
+		if (currentPage == viewFlipper.getChildCount()) {
+			displayRatio_normal(currentPage - 1);
+			currentPage = 0;
+			displayRatio_selelct(currentPage);
+		} else {
+			displayRatio_selelct(currentPage);
+			displayRatio_normal(currentPage - 1);
+		}
+		Log.e("currentPage", currentPage + "");		
+		
+	}
+	
+	private void showPreviousView(){
+		displayRatio_selelct(currentPage);
+		viewFlipper.setInAnimation(AnimationUtils.loadAnimation(this, R.anim.push_right_in));
+		viewFlipper.setOutAnimation(AnimationUtils.loadAnimation(this, R.anim.push_right_out));
+		viewFlipper.showPrevious();
+		currentPage --;
+		if (currentPage == -1) {
+			displayRatio_normal(currentPage + 1);
+			currentPage = viewFlipper.getChildCount() - 1;
+			displayRatio_selelct(currentPage);
+		} else {
+			displayRatio_selelct(currentPage);
+			displayRatio_normal(currentPage + 1);
+		}
+		Log.e("currentPage", currentPage + "");		
+	}
+	
+	private void displayRatio_selelct(int id){
+		int[] ratioId = {R.id.home_ratio_img_04, R.id.home_ratio_img_03, R.id.home_ratio_img_02, R.id.home_ratio_img_01};
+		ImageView img = (ImageView)findViewById(ratioId[id]);
+		img.setSelected(true);
+	}
+	
+	private void displayRatio_normal(int id){
+		int[] ratioId = {R.id.home_ratio_img_04, R.id.home_ratio_img_03, R.id.home_ratio_img_02, R.id.home_ratio_img_01};
+		ImageView img = (ImageView)findViewById(ratioId[id]);
+		img.setSelected(false);
+	}
+
+	private void toastInfo(String string){
+		Toast.makeText(MainMatch.this, string, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_match, menu);
+		return true;
+	}
+}
